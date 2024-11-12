dccafe9b072da05e552cdd2d86ac8fe181a7b285
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-12 15:15:11 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/dpbr/dpbrbe/DpbrBeApplication.java
@@ -0,0 +1,13 @@
+package com.dpbr.dpbrbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpbrBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpbrBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dpbr/dpbrbe/DpbrBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpbr.dpbrbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpbrBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
