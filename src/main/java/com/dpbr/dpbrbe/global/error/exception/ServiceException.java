0a6d0e4e4bc5fe721d4044cbd0508bc2f8cd2881
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-16 15:35:11 +0900

    feat: 특정 서비스 로직에서 발생할 수 있는 예외를 처리하는 ServiceException 클래스 추가
    - `ErrorCode`를 기반으로 메시지를 설정하고, 예외 발생 시 적절한 에러 코드를 전달하도록 구현

---
+++ src/main/java/com/dpbr/dpbrbe/global/error/exception/ServiceException.java
@@ -0,0 +1,15 @@
+package com.dpbr.dpbrbe.global.error.exception;
+
+import com.dpbr.dpbrbe.global.error.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class ServiceException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+     public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
