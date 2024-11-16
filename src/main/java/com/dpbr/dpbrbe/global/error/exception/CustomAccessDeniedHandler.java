ec5911ad28b943bfda73bdadb75aed91e33d191a
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-16 15:38:49 +0900

    feat: 권한이 없는 접근에 대해 403 Forbidden 응답을 반환하는 CustomAccessDeniedHandler 클래스 추가
    - `AccessDeniedException` 처리 시 JSON 형식의 `ErrorResponse` 반환
    - 응답 상태 코드와 메시지를 `ROLE_FORBIDDEN` 에러 코드로 설정

---
+++ src/main/java/com/dpbr/dpbrbe/global/error/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package com.dpbr.dpbrbe.global.error.exception;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.dpbr.dpbrbe.global.error.ErrorCode;
+import com.dpbr.dpbrbe.global.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        setResponse(response);
+    }
+
+    public void setResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResponse = mapper.writeValueAsString(
+                new ErrorResponse(ErrorCode.ROLE_FORBIDDEN));
+
+        response.getWriter().write(jsonResponse);
+    }
+}
