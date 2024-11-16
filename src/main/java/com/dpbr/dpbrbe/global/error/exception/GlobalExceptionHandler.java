273198b414d33d7ce43dd2401e71a8b668246cbb
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-16 16:06:48 +0900

    refactor: ErrorResponse의 생성 방식 변경으로 인한 코드 수정
    - 기존 `ErrorResponse` 생성 방식을 사용하는 코드들을 `ErrorResponse.of()` 메서드를 이용한 방식으로 변경

--- src/main/java/com/dpbr/dpbrbe/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dpbr/dpbrbe/global/error/exception/GlobalExceptionHandler.java
@@ -20,7 +20,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleServiceException(ServiceException e) {
         log.error(SERVICE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
 
-        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getHttpStatus()));
     }
 
@@ -28,7 +28,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
         log.error(UNEXPECTED_ERROR_LOG_FORMAT, e.getMessage(), e);
 
-        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
