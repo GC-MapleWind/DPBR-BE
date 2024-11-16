6ffac15623bad970404fd4c88493413e7fef5f9e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-16 15:36:32 +0900

    feat: 예외 처리 및 로깅을 위한 GlobalExceptionHandler 클래스 추가
    - `ServiceException`과 일반 `Exception`에 대한 예외 처리를 위한 메소드 추가
    - 예외 발생 시 적절한 HTTP 상태 코드와 `ErrorResponse`를 반환
    - `ServiceException` 발생 시 로깅, 예상치 못한 오류 발생 시 로깅 및 `500` 에러 처리

---
+++ src/main/java/com/dpbr/dpbrbe/global/error/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.dpbr.dpbrbe.global.error.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.dpbr.dpbrbe.global.error.ErrorCode;
+import com.dpbr.dpbrbe.global.error.ErrorResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    public static final String SERVICE_EXCEPTION_LOG_FORMAT = "ServiceException occurred: {}";
+    public static final String UNEXPECTED_ERROR_LOG_FORMAT = "Unexpected error occurred: {}";
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<ErrorResponse> handleServiceException(ServiceException e) {
+        log.error(SERVICE_EXCEPTION_LOG_FORMAT, e.getMessage(), e);
+
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getHttpStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
+        log.error(UNEXPECTED_ERROR_LOG_FORMAT, e.getMessage(), e);
+
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
