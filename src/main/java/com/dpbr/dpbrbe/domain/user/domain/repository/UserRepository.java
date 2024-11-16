252697b0932357adcdbf479e397399b168766589
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-16 16:04:31 +0900

    feat: UserRepository 인터페이스 추가
    - `JpaRepository`를 상속하여 `User` 엔티티에 대한 CRUD 작업 지원
    - `findByEmail` 메서드를 추가하여 이메일로 사용자를 조회할 수 있는 기능 제공

---
+++ src/main/java/com/dpbr/dpbrbe/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dpbr.dpbrbe.domain.user.domain.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dpbr.dpbrbe.domain.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
