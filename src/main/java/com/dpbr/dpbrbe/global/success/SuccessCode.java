d0428a16625fc55ae051576a8f45322a3642e46d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-17 17:14:45 +0900

    feat: 캐릭터 정보 업데이트 성공 코드 추가

--- src/main/java/com/dpbr/dpbrbe/global/success/SuccessCode.java
+++ src/main/java/com/dpbr/dpbrbe/global/success/SuccessCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum SuccessCode {
 
 	SUCCESS(200, "SUCCESS", "성공"),
+	UPDATE_CHARACTER_SUCCESS(200, "UPDATE_CHARACTER_SUCCESS", "캐릭터 정보를 성공적으로 업데이트했습니다."),
 	CREATED(201, "CREATED", "생성 완료"),
 
 	SAVE_CHARACTER_SUCCESS(201, "SAVE_CHARACTER_SUCCESS", "캐릭터를 성공적으로 저장했습니다.");
@@ -15,4 +16,4 @@ public enum SuccessCode {
 	private final int httpStatus;
 	private final String code;
 	private final String message;
-	}
+}
