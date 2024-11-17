cdbca04173e87f894c178002b9bbb49b94a3d1b5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-17 17:09:23 +0900

    feat: 캐릭터 정보 업데이트 기능 구현

---
+++ src/main/java/com/dpbr/dpbrbe/domain/character/usecase/UpdateInfoService.java
@@ -0,0 +1,31 @@
+package com.dpbr.dpbrbe.domain.character.usecase;
+
+import static com.dpbr.dpbrbe.global.success.SuccessCode.*;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.dpbr.dpbrbe.domain.character.domain.Character;
+import com.dpbr.dpbrbe.domain.character.domain.repository.CharacterRepository;
+import com.dpbr.dpbrbe.domain.character.exception.CharacterNotFoundException;
+import com.dpbr.dpbrbe.domain.character.presentation.dto.request.UpdateRequest;
+import com.dpbr.dpbrbe.global.openAPI.usecase.FetchCharacterInfo;
+import com.dpbr.dpbrbe.global.success.SuccessCode;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UpdateInfoService {
+
+	private final FetchCharacterInfo fetchCharacterInfo;
+	private final CharacterRepository characterRepository;
+
+	public SuccessCode execute(UpdateRequest request) throws IOException {
+		Character character = characterRepository.findByName(request.name()).orElseThrow(CharacterNotFoundException::new);
+		character.updateInfo(fetchCharacterInfo.execute(character.getOcid()));
+
+		return UPDATE_CHARACTER_SUCCESS;
+	}
+}
