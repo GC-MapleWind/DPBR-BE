6db7a194a1a82b0542d53f81b1efacdfe7e5067e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-17 16:43:28 +0900

    feat: 기본 성공 코드 추가

--- src/main/java/com/dpbr/dpbrbe/global/success/SuccessCode.java
+++ src/main/java/com/dpbr/dpbrbe/global/success/SuccessCode.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
+	SUCCESS(200, "SUCCESS", "성공"),
+	CREATED(201, "CREATED", "생성 완료"),
+
 	SAVE_CHARACTER_SUCCESS(201, "SAVE_CHARACTER_SUCCESS", "캐릭터를 성공적으로 저장했습니다.");
 
 	private final int httpStatus;
