c450b4f8ef124cf2faebfcff1decc0d6ad116645
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-17 17:10:18 +0900

    feat: 캐릭터 랭킹 기능 구현
    - `level`, `combatPower`, `unionLevel` 메서드에서 캐릭터의 랭킹을 반환하는 로직 추가

---
+++ src/main/java/com/dpbr/dpbrbe/domain/character/usecase/RankService.java
@@ -0,0 +1,42 @@
+package com.dpbr.dpbrbe.domain.character.usecase;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dpbr.dpbrbe.domain.character.domain.repository.CharacterRepository;
+import com.dpbr.dpbrbe.domain.character.presentation.dto.response.InfoResponse;
+import com.dpbr.dpbrbe.domain.character.presentation.dto.response.RankingResponse;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class RankService {
+
+	private final CharacterRepository characterRepository;
+
+	public List<RankingResponse> level() {
+		return characterRepository.findAllByOrderByLevelDesc().stream()
+			.map(character -> RankingResponse.of(
+				(long)(characterRepository.findAllByOrderByLevelDesc().indexOf(character) + 1),
+				InfoResponse.from(character)))
+			.toList();
+	}
+
+	public List<RankingResponse> combatPower() {
+		return characterRepository.findAllByOrderByCombatPowerDesc().stream()
+			.map(character -> RankingResponse.of(
+				(long)(characterRepository.findAllByOrderByLevelDesc().indexOf(character) + 1),
+				InfoResponse.from(character)))
+			.toList();
+	}
+
+	public List<RankingResponse> unionLevel() {
+		return characterRepository.findAllByOrderByUnionLevelDesc().stream()
+			.map(character -> RankingResponse.of(
+				(long)(characterRepository.findAllByOrderByLevelDesc().indexOf(character) + 1),
+				InfoResponse.from(character)))
+			.toList();
+	}
+}
