027ba129eb6d8a913bcc75b3d661c752008cd393
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-17 16:33:41 +0900

    feat: 컨트롤러에 사용자 학과 통계 기능 추가

--- src/main/java/com/dpbr/dpbrbe/domain/user/presentation/UserController.java
+++ src/main/java/com/dpbr/dpbrbe/domain/user/presentation/UserController.java
@@ -1,6 +1,7 @@
 package com.dpbr.dpbrbe.domain.user.presentation;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dpbr.dpbrbe.domain.user.presentation.dto.request.CharacterRequest;
 import com.dpbr.dpbrbe.domain.user.usecase.SaveCharacter;
+import com.dpbr.dpbrbe.domain.user.usecase.StatisticsService;
 import com.dpbr.dpbrbe.global.error.ErrorResponse;
 import com.dpbr.dpbrbe.global.response.GlobalResponseDto;
 import com.dpbr.dpbrbe.global.success.SuccessCode;
@@ -30,6 +32,7 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final SaveCharacter saveCharacter;
+	private final StatisticsService statisticsService;
 
 	@Operation(summary = "캐릭터 정보 저장", description = "사용자 캐릭터의 정보를 저장합니다.")
 	@ApiResponses({
@@ -47,4 +50,17 @@ public class UserController {
 			.body(GlobalResponseDto.success(saveCharacter.execute(userDetails, request)));
 	}
 
+	@Operation(summary = "학과 통계", description = "사용자들의 학과 통계를 산출합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200"),
+		@ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	@PostMapping("/statistics/major")
+	public ResponseEntity<GlobalResponseDto<Map<String, Integer>>> statisticsMajor() throws IOException {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success(statisticsService.major()));
+	}
 }
