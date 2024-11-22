15bff39e55180e5f44791c359228ddf2dca4bd21
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-23 01:27:54 +0900

    feat: 사용자의 캐릭터를 찾는 기능 구현

--- src/main/java/com/dpbr/dpbrbe/domain/user/presentation/UserController.java
+++ src/main/java/com/dpbr/dpbrbe/domain/user/presentation/UserController.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dpbr.dpbrbe.domain.character.presentation.dto.response.CharacterInfoResponse;
 import com.dpbr.dpbrbe.domain.user.presentation.dto.request.CharacterRequest;
+import com.dpbr.dpbrbe.domain.user.usecase.FindUserCharacter;
 import com.dpbr.dpbrbe.domain.user.usecase.SaveCharacter;
 import com.dpbr.dpbrbe.domain.user.usecase.StatisticsService;
 import com.dpbr.dpbrbe.global.error.ErrorResponse;
@@ -32,6 +35,7 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final SaveCharacter saveCharacter;
+	private final FindUserCharacter findUserCharacter;
 	private final StatisticsService statisticsService;
 
 	@Operation(summary = "캐릭터 정보 저장", description = "사용자 캐릭터의 정보를 저장합니다.")
@@ -62,5 +66,20 @@ public class UserController {
 	public ResponseEntity<GlobalResponseDto<Map<String, Integer>>> statisticsMajor() throws IOException {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(GlobalResponseDto.success(statisticsService.major(), SuccessCode.SUCCESS));
+	}
+
+	@Operation(summary = "로그인한 캐릭터 정보", description = "로그인한 사용자의 캐릭터 정보를 빈환합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200"),
+		@ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+		@ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+	})
+	@GetMapping("/my-character")
+	public ResponseEntity<GlobalResponseDto<CharacterInfoResponse>> findUserCharacter(
+		@AuthenticationPrincipal UserDetails userDetails) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success(findUserCharacter.execute(userDetails), SuccessCode.SUCCESS));
 	}
 }
---
+++ src/main/java/com/dpbr/dpbrbe/domain/user/usecase/FindUserCharacter.java
@@ -0,0 +1,37 @@
+package com.dpbr.dpbrbe.domain.user.usecase;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.dpbr.dpbrbe.domain.character.domain.Character;
+import com.dpbr.dpbrbe.domain.character.domain.repository.CharacterRepository;
+import com.dpbr.dpbrbe.domain.character.exception.CharacterNotFoundException;
+import com.dpbr.dpbrbe.domain.character.presentation.dto.response.CharacterInfoResponse;
+import com.dpbr.dpbrbe.domain.user.domain.User;
+import com.dpbr.dpbrbe.domain.user.domain.repository.UserRepository;
+import com.dpbr.dpbrbe.domain.user.exception.UserCharacterNotFoundException;
+import com.dpbr.dpbrbe.domain.user.exception.UserNotFoundException;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class FindUserCharacter {
+
+	private final UserRepository userRepository;
+	private final CharacterRepository characterRepository;
+
+	public CharacterInfoResponse execute(UserDetails userDetails) {
+		User user = userRepository.findByEmail(userDetails.getUsername()).orElseThrow(UserNotFoundException::new);
+
+		String ocid = user.getOcid();
+
+		if(ocid == null) {
+			throw new UserCharacterNotFoundException();
+		}
+
+		Character character = characterRepository.findById(ocid).orElseThrow(CharacterNotFoundException::new);
+
+		return CharacterInfoResponse.from(character);
+	}
+}
