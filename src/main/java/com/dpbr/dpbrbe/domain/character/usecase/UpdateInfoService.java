29b7404cc830e11a7026caf445e48312f4771d20
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-24 03:03:16 +0900

    feat: 매일 3시에 모든 캐릭터의 정보가 업데이트 되는 기능 추가

--- src/main/java/com/dpbr/dpbrbe/domain/character/usecase/UpdateInfoService.java
+++ src/main/java/com/dpbr/dpbrbe/domain/character/usecase/UpdateInfoService.java
@@ -4,6 +4,7 @@ import static com.dpbr.dpbrbe.global.success.SuccessCode.*;
 
 import java.io.IOException;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.dpbr.dpbrbe.domain.character.domain.Character;
@@ -23,11 +24,25 @@ public class UpdateInfoService {
 	private final CharacterRepository characterRepository;
 
 	public SuccessCode execute(UpdateRequest request) throws IOException {
-		Character character = characterRepository.findByName(request.name()).orElseThrow(CharacterNotFoundException::new);
+		Character character = characterRepository.findByName(request.name())
+			.orElseThrow(CharacterNotFoundException::new);
 		character.updateInfo(fetchCharacterInfo.execute(character.getOcid()));
 
 		characterRepository.save(character);
 
 		return UPDATE_CHARACTER_SUCCESS;
+	}
+
+	// 매일 새벽 3시에 실행
+	@Scheduled(cron = "0 0 3 * * ?")
+	public void executeDailyTask() {
+		characterRepository.findAll().parallelStream().forEach(character -> {
+			try {
+				character.updateInfo(fetchCharacterInfo.execute(character.getOcid()));
+				characterRepository.save(character);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
